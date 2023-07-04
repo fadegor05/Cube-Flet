aa3203cc351319c8f3817f70539b10f33619a015
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 10:22:49 +0400

    #2 commit
    added main function

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from config import *
+import flet as ft
+
+async def main(page : ft.Page):
+    page.title = TITLE
+    await page.add_async(ft.Text("It's CubeFlet"))
 
 if __name__ == '__main__':
-    pass
+    ft.app(target=main)
