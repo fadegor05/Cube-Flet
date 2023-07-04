d0d7ffa85295ea48bd21644035d57dbf9551a4c3
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 22:00:14 +0400

    #4 commit adding data loading and saving functions

---
+++ data.py
@@ -0,0 +1,11 @@
+import asyncio
+import json
+from config import *
+
+# Save async function
+async def save():
+    pass
+
+# Load async function
+async def load():
+    pass
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from config import *
 import asyncio
 
+# Main async function
 async def main():
     print("It's cubeflet")
 
 if __name__ == '__main__':
+    # Starting main async function
     asyncio.run(main())
